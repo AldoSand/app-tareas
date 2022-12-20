57ba5b39d2e6ab4be6dfd1dac2279aa49dfc5f0b
AldoSand <aldo_sand@outlook.com>
2022-12-20 15:41:39 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./componentes/Header";
+import FormularioTareas from "./componentes/FormularioTareas";
+import ListaTareas from "./componentes/ListaTareas";
 
-function App() {
+const App = () => {
+  const [tareas, cambiarTareas] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tareas", "Hola");
+  }, [tareas]);
+
+  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(false);
+  console.log(tareas);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <Header
+        mostrarCompletadas={mostrarCompletadas}
+        cambiarMostrarCompletadas={cambiarMostrarCompletadas}
+      />
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />
+      <ListaTareas
+        tareas={tareas}
+        cambiarTareas={cambiarTareas}
+        mostrarCompletadas={mostrarCompletadas}
+      />
     </div>
   );
-}
+};
 
 export default App;
